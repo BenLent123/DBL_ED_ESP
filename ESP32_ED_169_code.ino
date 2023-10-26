7d1a0a33dce91444b04e6922c4348daf39205e2f
Ben Lentschig <134686333+BenLent123@users.noreply.github.com>
2023-10-26 16:07:51 +0200

    Update ESP32_ED_169_code.ino

--- ESP32_ED_169_code.ino
+++ ESP32_ED_169_code.ino
@@ -6,12 +6,11 @@ const int pin1 = 23;         // pin for output
 const int pin2 = 22;         // pin for output
 int ESPBT_incoming;
 int direction; // 0 is left, 1 is both, 2 is right
-int number_of__vibrations; // 1-9 how many times we repeat the vibration
+int number_of__vibrations; // 1-5 how many times we repeat the vibration
 int duration; //duration of delay each vibration
 void setup() {
   Serial.begin(115200);
-  ESP_BT.begin("ESP32_169");
-
+  ESP_BT.begin("ESP32_169"); //name of ESP showing up on BT devices
 }
 
 
@@ -22,27 +21,27 @@ void vibrate() {
 
 if (direction == 0 ){                        
   for (int i = 0; i<number_of__vibrations;i++){
-    analogWrite(pin1,255);
+    analogWrite(pin1,255); //vibrate motor connected to pin1
     delay(duration * 300);
-    analogWrite(pin1,0);
+    analogWrite(pin1,0); // stop vibrating motor connected to pin1
     delay(300);
     
     }   
 }else if (direction == 1){
   for (int i = 0; i<number_of__vibrations;i++){
-    analogWrite(pin1,255);  
-    analogWrite(pin2,255); 
+    analogWrite(pin1,255);  //vibrate motor connected to pin1
+    analogWrite(pin2,255);  //vibrate motor connected to pin2
     delay(duration * 300);
-    analogWrite(pin1,0);
-    analogWrite(pin2,0);
+    analogWrite(pin1,0); //stop vibrating motor connected to pin1
+    analogWrite(pin2,0); //stop vibrating motor connected to pin2
     delay(300);
     
     }  
 }else if (direction == 2){
   for (int i = 0; i<number_of__vibrations;i++){
-    analogWrite(pin2,255);
+    analogWrite(pin2,255); //vibrate motor connected to pin1
     delay(duration * 300);
-    analogWrite(pin2,0);
+    analogWrite(pin2,0); //stop vibrating motor connected to pin2
     delay(300);
     
     }  
@@ -52,12 +51,12 @@ if (direction == 0 ){
 void loop() {
    if (ESP_BT.available())
   {
-    ESPBT_incoming = ESP_BT.read();
-    vibrate();
+    ESPBT_incoming = ESP_BT.read(); //read data incoming to ESP via bluetooth
+    vibrate(); // call vibration
     Serial.print("duration");
     Serial.println(duration);
     Serial.print("num vibe");
-    Serial.println(number_of__vibrations);
+    Serial.println(number_of__vibrations);          //ALL OF THIS IS JUST TO CHECK IF EVERYTHING IS FINE CAN BE DELETED!
     Serial.print("direction");
     Serial.println(direction);
     Serial.print("incoming");
